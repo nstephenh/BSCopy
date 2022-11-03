f1499a3aa2dd3388423c4a316658249c4888020a
Noah Stephen Haskell <nstephenh@gmail.com>
2022-11-03 10:36:31 -0500

    List nodes that will need manual mapping

--- map_cats.py
+++ map_cats.py
@@ -10,16 +10,18 @@ if __name__ == '__main__':
     ET.register_namespace("", "http://www.battlescribe.net/schema/catalogueSchema")
     tree = ET.parse(os.path.expanduser('~/BattleScribe/data/horus-heresy/2022 - LA Template.cattemplate'))
 
+    names_in_template = []
+
     # Find the base parts of the template
     for node in tree.findall("./{}s/{}".format(ENTRY_LINK_TYPE, ENTRY_LINK_TYPE)):
         bs_id = node.attrib.get("id")
         name = node.attrib.get("name")
         if name and bs_id:
             name_map[get_identifier(node)] = bs_id
-        if name and not bs_id:
-            print("{} has name but no ID: {}".format(node, name))
-        if bs_id and not name:
-            print("{} has ID but no Name: {}".format(node, bs_id))
+            if name in names_in_template:
+                print("{} needs manually mapped due to multiple entries".format(name))
+            else:
+                names_in_template.append(name)
 
     base_path = os.path.expanduser('~/BattleScribe/data/horus-heresy')
     la_files = os.listdir(base_path)
