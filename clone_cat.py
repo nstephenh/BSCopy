4d4eefce90f9bad46651da0c2d52b5ddc51ef6c1
Noah Stephen Haskell <nstephenh@gmail.com>
2022-11-03 10:36:10 -0500

    Make generate_template_from_base.py not generate map comments.

--- clone_cat.py
+++ clone_cat.py
@@ -8,7 +8,7 @@ from util import update_all_node_ids, add_new_id
 
 # Press the green button in the gutter to run the script.
 
-def clone_cat(output_name, assign_ids_to_mods_and_cons=False):
+def clone_cat(output_name, assign_ids_to_mods_and_cons=False, generate_map_comments=True):
     node_map = {}  # Dictionary of input IDs to output IDs
     ET.register_namespace("", "http://www.battlescribe.net/schema/catalogueSchema")
     tree = ET.parse(os.path.expanduser('~/BattleScribe/data/horus-heresy/2022 - LA - Blood Angels.cat'))
@@ -18,7 +18,9 @@ def clone_cat(output_name, assign_ids_to_mods_and_cons=False):
     for node in tree.iter():
         add_new_id(node_map, node)
 
-    update_all_node_ids(tree2.iter(), node_map, assign_ids_to_mods_and_cons=assign_ids_to_mods_and_cons)
+    update_all_node_ids(tree2.iter(), node_map,
+                        generate_map_comments=generate_map_comments,
+                        assign_ids_to_mods_and_cons=assign_ids_to_mods_and_cons)
 
     tree2.write(os.path.expanduser('~/BattleScribe/data/horus-heresy/{}'.format(output_name)))
 
--- generate_template_from_base.py
+++ generate_template_from_base.py
@@ -1,7 +1,7 @@
 from clone_cat import clone_cat
 
 if __name__ == '__main__':
-    clone_cat("2022 - LA Template.cat", assign_ids_to_mods_and_cons=True)
+    clone_cat("2022 - LA Template.cat", assign_ids_to_mods_and_cons=True, generate_map_comments=False)
     print("""
 A copy of the blood angels cat file has been made. 
 Please edit it to remove blood angels generics and make any other basic changes, 
