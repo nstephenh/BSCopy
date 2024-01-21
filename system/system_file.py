fa9373b04da5432a21f7ad2aa6dc911f11e9e469
Noah Stephen Haskell <nstephenh@gmail.com>
2024-01-20 18:47:40 -0600

    Untested code to create a new element

--- system/system_file.py
+++ system/system_file.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from xml.etree import ElementTree as ET
 
 from system.node import Node
+from util.generate_util import get_random_bs_id
 
 if TYPE_CHECKING:
     from system.system import System
@@ -33,25 +34,57 @@ class SystemFile:
         self.parent_map = {c: p for p in self.source_tree.iter() for c in p}
 
         for element in self.source_tree.findall('.//*[@id]'):
-            node = Node(self, element)
-            self.nodes_by_id.update({node.id: node})
+            self.create_node_from_element(element)
 
-            if node.tag not in self.nodes_by_type.keys():
-                self.nodes_by_type[node.tag] = []
-            self.nodes_by_type[node.tag].append(node)
+    def create_node_from_element(self, element):
+        """
+        Creates a node and adds it to our indexes.
+        :param element:
+        :return:
+        """
+        node = Node(self, element)
+        self.nodes_by_id.update({node.id: node})
 
-            if node.name:
-                if node.name not in self.nodes_by_name.keys():
-                    self.nodes_by_name[node.name] = []
-                self.nodes_by_name[node.name].append(node)
+        if node.tag not in self.nodes_by_type.keys():
+            self.nodes_by_type[node.tag] = []
+        self.nodes_by_type[node.tag].append(node)
 
-            if node.target_id:
-                if node.target_id not in self.nodes_by_name.keys():
-                    self.nodes_by_target_id[node.target_id] = []
-                self.nodes_by_target_id[node.target_id].append(node)
+        if node.name:
+            if node.name not in self.nodes_by_name.keys():
+                self.nodes_by_name[node.name] = []
+            self.nodes_by_name[node.name].append(node)
+
+        if node.target_id:
+            if node.target_id not in self.nodes_by_name.keys():
+                self.nodes_by_target_id[node.target_id] = []
+            self.nodes_by_target_id[node.target_id].append(node)
 
     def get_namespace_tag(self) -> str:
         return "{" + self.namespace + "}"
+
+    def create_element(self, tag, name, parent=None, pub=None, page=None, ):
+        """
+        UNTESTED
+        Creates a new element with ID, and adds it to the appropriate indexes. If
+        :param tag:
+        :param name:
+        :param parent: defaults to shared<tag>s
+        :param pub:
+        :param page:
+        :return:
+        """
+        if parent is None:
+            shared_element_root_name = f"shared{tag[0].upper()}{tag[1:]}s"
+            parent = self.source_tree.getroot().find(shared_element_root_name)
+            if parent is None:
+                raise Exception(f"Cannot find {shared_element_root_name} in {self.name}")
+
+        new_element = ET.SubElement(parent, tag, attrib={'name': name, 'hidden': "false",
+                                                         'id': get_random_bs_id(), 'page': page,
+                                                         'publicationId': pub})
+        self.create_node_from_element(new_element)
+        self.parent_map[parent] = new_element
+        return new_element
 
 
 def get_namespace_from_file(filename: str):
