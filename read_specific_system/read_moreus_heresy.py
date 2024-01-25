18a9b8449ba3a3189ca8e4cf7f73fdddbdb972b6
Noah Stephen Haskell <nstephenh@gmail.com>
2024-01-25 11:17:43 -0600

    specific read scripts for each game so I don't have to change things back and forth all the time.

---
+++ read_specific_system/read_moreus_heresy.py
@@ -0,0 +1,19 @@
+from book_reader.constants import ReadSettingsKeys, Actions
+from system.constants import SpecialRulesType, SystemSettingsKeys, GameImportSpecs
+from system.system import System
+
+if __name__ == '__main__':
+    system = System('moreus-heresy',
+                    settings={
+                        SystemSettingsKeys.GAME_IMPORT_SPEC: GameImportSpecs.HERESY,
+                    },
+                    include_raw=True,
+                    raw_import_settings={
+                        ReadSettingsKeys.FIRST_PARAGRAPH_IS_FLAVOR: True,
+                        ReadSettingsKeys.ACTIONS: [
+                            Actions.DUMP_TO_JSON,
+                            Actions.LOAD_SPECIAL_RULES,
+                            Actions.LOAD_WEAPON_PROFILES,
+                        ],
+                    })
+    system.save_system()
---
+++ read_specific_system/read_the_old_world.py
@@ -0,0 +1,21 @@
+from book_reader.constants import ReadSettingsKeys, Actions
+from system.constants import SpecialRulesType, SystemSettingsKeys, GameImportSpecs
+from system.system import System
+
+if __name__ == '__main__':
+    system = System('Warhammer-The-Old-World',
+                    settings={
+                        SystemSettingsKeys.SPECIAL_RULE_TYPE: SpecialRulesType.PROFILE_SPECIAL_RULE,
+                        SystemSettingsKeys.WEAPON_AS_DESCRIPTION: True,
+                        SystemSettingsKeys.GAME_IMPORT_SPEC: GameImportSpecs.OLDWORLD,
+                    },
+                    include_raw=True,
+                    raw_import_settings={
+                        ReadSettingsKeys.FIRST_PARAGRAPH_IS_FLAVOR: True,
+                        ReadSettingsKeys.ACTIONS: [
+                            Actions.DUMP_TO_JSON,
+                            Actions.LOAD_SPECIAL_RULES,
+                            Actions.LOAD_WEAPON_PROFILES,
+                        ],
+                    })
+    system.save_system()
