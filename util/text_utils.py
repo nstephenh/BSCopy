nstephenh/BSCopy

commit 6fc15922260a606dc8b1589edf5ea4dfb58065a9

Fix pulling special rules from heresy

util/text_utils.py
@@ -283,6 +283,15 @@ def split_at_header(header, datasheet_text, header_at_end_of_line=True) -> (bool
 
 
 def split_after_header(raw_text, header):
+    """
+    Splits after an indented header, such as:
+    Special Rules:  Some items
+                    More items
+    Will split out after more items, and not just on the line "Special Rules"
+    :param raw_text:
+    :param header:
+    :return:
+    """
     header_spacing = 0
     lines = raw_text.split("\n")
     for index, line in enumerate(lines):
@@ -337,3 +346,17 @@ def un_justify(text):
         else:
             new_text_array.append(line[index:])
     return "\n".join(new_text_array)
+
+
+def get_first_non_list_or_header_line(text, headers):
+    """
+    Get the first non-list entry or header line after the initial line.
+    :param text:
+    :param headers:
+    :return:
+    """
+    for index, line in enumerate(text.splitlines()[1:]):
+        if line.strip() == "":
+            continue
+        if not (line.lstrip()[0] in bullet_options or line.strip() in headers):
+            return index + 1

book_reader/page.py
@@ -24,6 +24,7 @@ class Page:
         self.units_text: list[str] = []
         self.units: list[RawUnit] = []
         self.page_type = self.get_configured_page_type()
+        self.special_rules_text = None
 
     @property
     def settings(self) -> dict[ReadSettingsKeys: str | dict]:
@@ -43,7 +44,7 @@ class Page:
             'Units': [
                 unit.serialize() for unit in self.units
             ],
-            # "Special Rules Text": self.special_rules_text,
+            "Special Rules Text": self.special_rules_text,
             'Special Rules': self.special_rules_dict
         }
 

book_reader/pdf_page.py
@@ -13,7 +13,6 @@ class PdfPage(Page):
 
     def __init__(self, book, raw_text, page_number, prev_page_type=None):
         super().__init__(book, page_number)
-        self.special_rules_text = None
         self.raw_text = raw_text
         self.page_number = page_number
         if self.raw_text.strip() == "":
@@ -160,8 +159,7 @@ class PdfPage(Page):
     def get_text_unit(self, rules_text):
         profile_locator = self.game.ProfileLocator
 
-        # First, try and split this datasheet into parts based on known headers
-        if not self.game.MIDDLE_IN_2_COLUMN:
+        if not self.game.MIDDLE_IN_2_COLUMN:  # This block is for heresy, which ends with the special rules section
             if self.game.ENDS_AFTER_SPECIAL_RULES:
                 rules_text, self.special_rules_text = split_after_header(rules_text, "Special Rules:")
             return rules_text
@@ -170,20 +168,48 @@ class PdfPage(Page):
         was_split, profiles, upper_half = split_at_header(profile_locator, upper_half, header_at_end_of_line=False)
         if not was_split:
             print(f"Could not split at {profile_locator}")
-            return  # If this datasheet doesn't have "Unit composition, something is wrong
+            return  # If this datasheet doesn't have Unit composition, something is wrong
 
-        # Access points comes before Options, though a sheet is not guaranteed to have either.
-        was_split, upper_half, lower_half = split_at_header("Access Points", upper_half)
-        if not was_split:
-            _, upper_half, lower_half = split_at_header(self.book.system.game.OPTIONS, upper_half)
+        print_styled("Upper Half", STYLES.GREEN)
+        _, upper_half, wargear_and_on = split_at_header("Wargear", upper_half, header_at_end_of_line=False)
 
-        upper_half, comp_and_wargear, type_and_special_rules, _ = split_into_columns(upper_half, debug_print_level=0)[0]
+        # Go through sections in reverse order now:
+        headers = ["Dedicated Transport:",
+                   "Access Points:",
+                   "Options:"
+                   ]
+        end_of_bullets = text_utils.get_first_non_list_or_header_line(wargear_and_on, headers)
+        lines = wargear_and_on.splitlines()
+        if end_of_bullets:  # Otherwise, there will be no options, access points, dedicated transport, etc
+            self.special_rules_text = "\n".join(lines[end_of_bullets:])
+            wargear_and_on = "\n".join(lines[:end_of_bullets])
+            print_styled(f"Lines from wargear and special rules + the following sections: {headers}", STYLES.GREEN)
+            print(wargear_and_on)
+            print_styled("Special Rules", STYLES.GREEN)
+            print(self.special_rules_text)
+
+        # At this point wargear and no longer has any special rules or profiles.
+        upper_half = "\n".join(upper_half.splitlines() + wargear_and_on.splitlines())
+        print_styled("Upper Half without any special rules text", STYLES.GREEN)
+        print(upper_half)
+
+        # progressively split wargear and on in reverse order, till we get back up to just the wagear and special rules
+        header_sections = {}
+        for header in reversed(headers):
+            was_split, wargear_and_on, content = split_at_header(header, wargear_and_on)
+            if was_split:
+                header_sections[header] = content
+
+        upper_half, comp_and_wargear, type_and_special_rules, _ = \
+            split_into_columns(wargear_and_on, debug_print_level=0)[0]
 
         # Now lets put everything together:
         print_styled("Reconstructed Datasheet", STYLES.GREEN)
-        return "".join(
-            [profiles, comp_and_wargear, type_and_special_rules, upper_half, lower_half]
+        new_text = "".join(
+            [profiles, comp_and_wargear, type_and_special_rules, upper_half] + [header_sections[header] for header in
+                                                                                header_sections.keys()]
         )
+        return new_text
 
     def split_before_line_before_statline(self, raw_text):
         """

commit e9ee1c4b1330e0067f82fbcf83f99e5a2018812d

Fix split_after_header, previously broken by 6fc15922260a606dc8b1589edf5ea4dfb58065a9

util/text_utils.py
@@ -296,13 +296,15 @@ def split_after_header(raw_text, header):
     lines = raw_text.split("\n")
     for index, line in enumerate(lines):
         if line.startswith(header):
-            line = get_line_indent(line, len(header))
+            # print(f"Header line: {header_spacing} ⎹\t{line}")
+            header_spacing = get_line_indent(line, len(header))
             continue
         if header_spacing:
             # Line is indented as part of table
             line_spacing = get_line_indent(line)
             if line_spacing != header_spacing:
                 return "\n".join(lines[:index]), "\n".join(lines[index:])
+            # print(f"{header_spacing} vs {line_spacing} ⎹\t{line}")
     return raw_text, ""
 
 
@@ -363,3 +365,4 @@ def get_line_indent(line, offset=0):
     for i, char in enumerate(line[offset:]):  # Skip the bullet
         if char != " ":
             return i + offset
+    return 0