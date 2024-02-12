0f45489d5eb93e8220bb23797b5238a105091285
Noah Stephen Haskell <nstephenh@gmail.com>
2024-02-12 14:02:26 -0600

    Change file IDs option

--- ensure_links_point_properly.py
+++ ensure_links_point_properly.py
@@ -2,13 +2,22 @@
 # If you've forked a repo, then you need to change the IDs for the gst and each cat file.
 # This script can be run to notify you if any of the cat files ID's need to be changed
 # and updates any catalogue links referencing the old IDs.
+#
 import os.path
 import re
 import xml.etree.ElementTree as ET
 
+from util.generate_util import get_random_bs_id
+
+if __name__ == '__main__':
+    pass  # Putting this up here will cause the rest of the script to run without indenting it.
+
 bsdata_source = os.path.expanduser('~/BattleScribe/data/')
 upstream_game = "horus-heresy"
 downstream_game = "moreus-heresy"
+
+change_ids = True  # If a root ID is the same, should we change it?
+change_full_id = False  # If False, will only update the last digit of the ID.
 
 upstream_dir = os.path.join(bsdata_source, upstream_game)
 downstream_dir = os.path.join(bsdata_source, downstream_game)
@@ -35,14 +44,28 @@ for file_name in os.listdir(downstream_dir):
         continue
     tree = ET.parse(file)
     new_id_map[file_name] = tree.getroot().attrib['id']
-keys_to_pop = []
-for key in original_id_map.keys():
-    if original_id_map[key] == new_id_map[key]:
-        print(f"\tKey {key} needs to be changed")
-        keys_to_pop.append(key)
+    if original_id_map[file_name] == new_id_map[file_name] and change_ids:
+        if change_full_id:
+            new_id = get_random_bs_id()
+        else:
+            old_last_digit = new_id_map[file_name][-1]
+            new_last_digit = hex(int(old_last_digit, 16) + 1)[-1]
+            new_id = new_id_map[file_name][:-1] + new_last_digit
 
-for key in keys_to_pop:
-    original_id_map.pop(key)  # prevent errors printing about this key
+        print(f"\tFile {file_name} should get a new id")
+        print(f"\t\tOld ID: {original_id_map[file_name]}, New ID: {new_id}")
+        tree.getroot().attrib['id'] = new_id
+        new_id_map[file_name] = new_id
+
+if not change_ids:
+    keys_to_pop = []
+    for key in original_id_map.keys():
+        if original_id_map[key] == new_id_map[key]:
+            print(f"\tKey {key} needs to be changed")
+            keys_to_pop.append(key)
+
+    for key in keys_to_pop:
+        original_id_map.pop(key)  # prevent errors printing about this key
 
 print("Looking for occurrences of old IDs")
 for file_name in os.listdir(downstream_dir):
