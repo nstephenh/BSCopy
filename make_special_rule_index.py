fa6a92e7d1d4150647eab00c4e5d83761d4ee0db
Noah Stephen Haskell <nstephenh@gmail.com>
2024-07-09 13:47:58 -0500

    Put exported info into exports directory.

--- make_special_rule_index.py
+++ make_special_rule_index.py
@@ -12,7 +12,7 @@ from util.log_util import print_styled, STYLES, get_diff, prompt_y_n
 
 if __name__ == '__main__':
 
-    system = System('moreus-heresy',
+    system = System('horus-heresy-panoptica',
                     settings={
                         SystemSettingsKeys.GAME_IMPORT_SPEC: GameImportSpecs.HERESY,
                     },
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     for node in system.gst.root_node.get_child("publications").children:
         publication_names[node.id] = node.name
 
-    with open('rules.csv', 'w', newline="") as csvfile:
+    with open('exports/rules.csv', 'w', newline="") as csvfile:
 
         writer = csv.DictWriter(csvfile, fieldnames=["name", "page", "publication"])
         writer.writeheader()
@@ -39,12 +39,29 @@ if __name__ == '__main__':
                 rule_as_dict["publication"] = publication_names[pub]
             writer.writerow(rule_as_dict)
 
-    with open('wargear.csv', 'w', newline="") as csvfile:
+    with open('exports/wargear.csv', 'w', newline="") as csvfile:
 
         writer = csv.DictWriter(csvfile, fieldnames=["name", "page", "publication"])
         writer.writeheader()
 
         for rule in system.nodes_with_ids.filter(lambda x: x.type == "profile:Wargear Item"):
+            if rule.name is None:
+                continue  # Not sure why there's some blanks here, maybe links are being counted?
+            rule_as_dict = {
+                "name": rule.name,
+                "page": rule.attrib.get('page'),
+            }
+            pub = rule.attrib.get('publicationId')
+            if pub is not None:
+                rule_as_dict["publication"] = publication_names[pub]
+            writer.writerow(rule_as_dict)
+
+    with open('exports/weapons.csv', 'w', newline="") as csvfile:
+
+        writer = csv.DictWriter(csvfile, fieldnames=["name", "page", "publication"])
+        writer.writeheader()
+
+        for rule in system.nodes_with_ids.filter(lambda x: x.type == "profile:Weapon"):
             if rule.name is None:
                 continue  # Not sure why there's some blanks here, maybe links are being counted?
             rule_as_dict = {
