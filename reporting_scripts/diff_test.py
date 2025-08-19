ecd9654c72b68d0724897f868c9323e04108741b
Noah Stephen Haskell <nstephenh@gmail.com>
2025-08-18 21:59:14 -0500

    Vaguely describe what's changed

--- reporting_scripts/diff_test.py
+++ reporting_scripts/diff_test.py
@@ -1,13 +1,30 @@
 # Force python XML parser not faster C accelerators
 # because we can't hook the C implementation
+import argparse
+import difflib
+import os
 import sys
 
+from git import Repo
+
 sys.modules['_elementtree'] = None
+print(os.getcwd())
+sys.path.insert(1, os.getcwd() + "/BSCopy")
 
 from system.constants import SystemSettingsKeys, GameImportSpecs
 from system.system import System
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-merge_base")
+    args = parser.parse_args()
+
+    merge_base = args.merge_base
+
+    if args.merge_base is None:  # for testing
+        merge_base = "f8adfcf7dc5b689f51c70c6dd99c8c47e22e4704"
+
     system = System('horus-heresy-3rd-edition',
                     settings={
                         SystemSettingsKeys.GAME_IMPORT_SPEC: GameImportSpecs.HERESY3E,
@@ -17,3 +34,47 @@ if __name__ == '__main__':
     crusade = system.get_node_by_id("8562-592c-8d4b-a1f0")
 
     print(crusade.start_line_number)
+    repo = Repo(system.game_system_location)
+    head_commit = list(repo.iter_commits())[0]
+    main_commit = None
+    for commit in repo.iter_commits():
+        if commit.hexsha == merge_base:
+            main_commit = commit
+            break
+    print(main_commit)
+    diff_index = main_commit.diff(head_commit)
+    for diff_item in diff_index:
+        print(diff_item.a_path)
+        if not (diff_item.a_path.endswith('.cat') or diff_item.a_path.endswith('.gst')):
+            continue
+        a_file = diff_item.a_blob.data_stream.read().decode('utf-8')
+        b_file = diff_item.b_blob.data_stream.read().decode('utf-8')
+        diff_lib = difflib.Differ()
+        a_count = 0
+        b_count = 0
+        removed_things = []
+        added_or_modified_nodes = []
+
+        system_file = None
+        for file in system.files:
+            if file.name == diff_item.a_path:
+                system_file = file
+                break
+        if system_file is None:
+            print(f"Could not find file for {diff_item.a_path}")
+        for line in diff_lib.compare(a_file.splitlines(), b_file.splitlines()):
+            if line.startswith('  '):
+                a_count += 1
+                b_count += 1
+            elif line.startswith('- '):
+                a_count += 1
+                print(a_count, line)
+            elif line.startswith('+ '):
+                b_count += 1
+                print(b_count, line)
+                node_candidates = system_file.all_nodes.filter(lambda x: x.start_line_number == b_count)
+                if len(node_candidates) == 1:
+                    print(str(node_candidates[0]))
+
+
+
