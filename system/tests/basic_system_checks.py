d6250ade48bcdd3b88a8d85346fea17bfa9b249e
Noah Stephen Haskell <nstephenh@gmail.com>
2025-08-28 17:06:34 -0500

    Don't use deprecated function

--- system/tests/basic_system_checks.py
+++ system/tests/basic_system_checks.py
@@ -12,7 +12,7 @@ class BasicSystemTests(unittest.TestCase):
     def setUp(self):
         game_name = os.getenv("SYSTEM_NAME")
         self.assertIsNotNone(game_name, "Missing SYSTEM_NAME environment variable")
-        self.assertNotEquals(game_name, "", "Missing SYSTEM_NAME environment variable")
+        self.assertNotEqual(game_name, "", "Missing SYSTEM_NAME environment variable")
         self.system = System(game_name)
 
     def test_system_exists(self):
